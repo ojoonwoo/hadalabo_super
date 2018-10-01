007a327405907e1835971808d3e217e10263fc36
ojw <ojoonwoo@gmail.com>
2018-10-01 19:05:11 +0900

    first commit

---
+++ include/mnv_dbi.php
@@ -0,0 +1,24 @@
+<?
+/*
+*
+*	DB 연결 정보
+*
+*/
+class mnv_dbi
+{
+	var $my_db;
+	public function Connect_MySQL()
+	{
+//		$my_db = new mysqli("localhost", "root", "alslqj~1", "viral4373");
+		$my_db = new mysqli("localhost", "root", "alslqj~1", "h_capital");
+		// $my_db = new mysqli("localhost", "viral4373", "alslqj_2017", "viral4373");
+
+		if (mysqli_connect_error()) {
+			exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+		}
+
+		mysqli_query ($my_db,"set names utf8");
+
+		return $my_db;
+	}
+}
