6e0baa43836f196b190e03ca30f68dc85cafdfd1
kyhfan <kyhfan@gmail.com>
2018-10-08 12:16:35 +0900

    하다라보 모바일

--- m/event.php
+++ m/event.php
@@ -3,101 +3,94 @@
 ?>
 	<body>
 		<div class="wrap">
-			<div class="event-section event-section--01">
-				<div class="event-section__contents">
-					<h1 class="title">
-						<img src="images/event-section-01-title.png" alt="하다라보 고쿠쥰 4행시 이벤트 내가 쓰는 수퍼보습">
-					</h1>
-					<p class="text">
-						<img src="images/event-section-01-text.png" alt="">
-						<span class="for-a11y">
-							3중 히알루론산 황금비율로 더 깊어진 보습이라니, 수퍼~보습 내가 쓰는 4행시가 하다라보 광고가 된다니, 수퍼~서프라이즈 지금 ‘수퍼보습’으로 4행시에 도전하세요. 참여만 해도 [수퍼보습 5days KIT] 100% 겟!
-						</span>
-					</p>
-					<ul class="form">
-						<li class="form-01">
-							<span class="for-a11y">[수]</span>
-							<input type="text" id="quatrain01" class="quatrain-input" title="4행시 중 [수]에 대한 내용">
-							<label for="quatrain01">13자 이내로 입력해주세요</label>
-						</li>
-						<li class="form-02">
-							<span class="for-a11y">[퍼]</span>
-							<input type="text" id="quatrain02" class="quatrain-input" title="4행시 중 [퍼]에 대한 내용">
-							<label for="quatrain02">13자 이내로 입력해주세요</label>
-						</li>
-						<li class="form-03">
-							<span class="for-a11y">[보]</span>
-							<input type="text" id="quatrain03" class="quatrain-input" title="4행시 중 [보]에 대한 내용">
-							<label for="quatrain03">13자 이내로 입력해주세요</label>
-						</li>
-						<li class="form-04">
-							<span class="for-a11y">[습]</span>
-							<input type="text" id="quatrain04" class="quatrain-input" title="4행시 중 [습]에 대한 내용">
-							<label for="quatrain04">13자 이내로 입력해주세요</label>
-						</li>
-						<li class="form-05">
-							<span class="for-a11y">by</span>
-							<input type="text" id="quatrain_name" title="작성자 이름">
-							<label for="quatrain_name">작성자 이름</label>
-						</li>
-					</ul>
-					<ul class="sns">
-						<li class="sns-01">
-							<button type="button">
-								<img src="images/event-section-01-sns-01.png" alt="페이스북으로 공유하기">
-							</button>
-						</li>
-						<li class="sns-02">
-							<button type="button">
-								<img src="images/event-section-01-sns-02.png" alt="트위터로 공유하기">
-							</button>
-						</li>
-						<li class="sns-03">
-							<button type="button">
-								<img src="images/event-section-01-sns-03.png" alt="카카오 톡으로 공유하기">
-							</button>
-						</li>
-						<li class="sns-04">
-							<button type="button">
-								<img src="images/event-section-01-sns-04.png" alt="카카오 스토리로 공유하기">
-							</button>
-						</li>
-					</ul>
-					<!-- <button type="button" class="event-submit" onclick="hadalaboUI.layer.open($('#layerEntry'));"> -->
-					<button type="button" class="event-submit"><!-- 20180930 : onclick 추가 -->
-						<img src="images/event-section-01-submit.png" alt="등록하기">
-					</button>
-				</div>
-			</div>
-			<div class="footer footer--event">
+			<section class="event-section event-section--01">
+				<h1 class="title">
+					<img src="images/event-section-01-title.png" alt="하다라보 고쿠쥰 4행시 이벤트 내가 쓰는 수퍼보습" class="content-image">
+				</h1>
+				<p class="text">
+					<img src="images/event-section-01-text.png" alt="" class="content-image">
+					<span class="for-a11y">
+						지금 ‘수퍼보습’으로 4행시에 도전하세요.<br>
+						참여만 해도 [수퍼보습 5days KIT] 100% 겟!
+					</span>
+				</p>
+				<ul class="form">
+					<li class="form-01">
+						<span class="for-a11y">[수]</span>
+						<input type="text" id="quatrain01" class="quatrain-input" title="4행시 중 [수]에 대한 내용" placeholder="13자 이내로 입력해주세요">
+					</li>
+					<li class="form-02">
+						<span class="for-a11y">[퍼]</span>
+						<input type="text" id="quatrain02" class="quatrain-input" title="4행시 중 [퍼]에 대한 내용" placeholder="13자 이내로 입력해주세요">
+					</li>
+					<li class="form-03">
+						<span class="for-a11y">[보]</span>
+						<input type="text" id="quatrain03" class="quatrain-input" title="4행시 중 [보]에 대한 내용" placeholder="13자 이내로 입력해주세요">
+					</li>
+					<li class="form-04">
+						<span class="for-a11y">[습]</span>
+						<input type="text" id="quatrain04" class="quatrain-input" title="4행시 중 [습]에 대한 내용" placeholder="13자 이내로 입력해주세요">
+					</li>
+					<li class="form-05">
+						<span class="for-a11y">by</span>
+						<input type="text" id="quatrain_name" title="작성자 이름" placeholder="작성자 이름">
+					</li>
+				</ul>
+				<!-- <button type="button" class="event-submit" onclick="hadalaboUI.layer.open($('#layerEntry'));"> -->
+				<button type="button" class="event-submit">
+					<img src="images/event-section-01-submit.png" alt="등록하기">
+				</button>
+				<ul class="sns">
+					<li class="sns-01">
+						<button type="button">
+							<img src="images/event-section-01-sns-01.png" alt="페이스북으로 공유하기">
+						</button>
+					</li>
+					<li class="sns-02">
+						<button type="button">
+							<img src="images/event-section-01-sns-02.png" alt="트위터로 공유하기">
+						</button>
+					</li>
+					<li class="sns-03">
+						<button type="button">
+							<img src="images/event-section-01-sns-03.png" alt="카카오 톡으로 공유하기">
+						</button>
+					</li>
+					<li class="sns-04">
+						<button type="button">
+							<img src="images/event-section-01-sns-04.png" alt="카카오 스토리로 공유하기">
+						</button>
+					</li>
+				</ul>
+			</section>
+			<footer class="footer">
 				<h1 class="logo">
-					<img src="images/footer-logo--event.png" alt="멘소래담">
+					<img src="images/footer-logo.png" alt="멘소래담">
 				</h1>
 				<address>
-					<strong>한국맨소래담 ㈜</strong>  우)135-080 서울특별시 강남구 언주로 337. 동영문화센터 7층(역삼동)
+					<strong>한국맨소래담</strong> ㈜ &nbsp;우)135-080 서울특별시 강남구 언주로 337. 동영문화센터 7층(역삼동)
 				</address>
 				<p>
-					전화번호 : 02-717-1821 &nbsp;&nbsp;ㅣ&nbsp;&nbsp; 팩스번호 : 02-717-0833 &nbsp;&nbsp;ㅣ&nbsp;&nbsp; 사업자등록번호 : 102-84-03171 &nbsp;&nbsp;ㅣ&nbsp;&nbsp; 대표이사 : 신카이청마이클
+					전화번호 : 02-717-1821 &nbsp;&nbsp;ㅣ&nbsp;&nbsp; 팩스번호 : 02-717-0833<br>
+					사업자등록번호 : 102-84-03171 &nbsp;&nbsp;ㅣ&nbsp;&nbsp; 대표이사 : 신카이청마이클
 				</p>
 				<address class="copy">
 					COPYRIGHT&copy;2016 MENTHOLATUM. ALL RIGHTS RESERVED.
 				</address>
-			</div>
+			</footer>
 		</div>
 
-		<!-- 20180930 : layer 추가 -->
-
 		<!-- layer : 응모하기 -->
-		<div class="layer-wrap layer-entry" id="layerEntry">
-			<div class="layer layer--medium">
+		<div class="layer-wrap layer-entry" id="layerEntry" data-layer-options="documentClose: false;">
+			<section class="layer layer--medium">
 				<button type="button" class="layer-close js-layer-close">
 					<span class="for-a11y">이벤트 참여하기 레이어 팝업 닫기</span>
 				</button>
 				<h1 class="title">
-					<img src="images/layer-entry-title.png" alt="참여에 감사드립니다!">
+					<img src="images/layer-entry-title.png" alt="참여에 감사드립니다!" class="popup-image">
 				</h1>
 				<p class="text">
-					<img src="images/layer-entry-text.png" alt="">
+					<img src="images/layer-entry-text.png" alt="" class="popup-image">
 					<span class="for-a11y">
 						[수퍼보습 5days KIT] 통해 더 깊어진 하다라보 고쿠쥰의 힘을 느껴보세요!
 					</span>
@@ -128,7 +121,7 @@
 					</dt>
 					<dd>
 						<!-- <input type="text" id="layerEntryAddress" class="address-01" title="주소1"> -->
-						<input type="text" id="mb_addr1" class="address-01" title="주소1" readonly>
+						<input type="text" id="mb_addr1" class="address-01" title="주소1">
 						<button type="button" class="search">
 							<img src="images/layer-entry-search.png" alt="우편번호 검색">
 						</button>
@@ -141,7 +134,7 @@
 					<label for="layerEntryAgree">
 						<img src="images/layer-entry-agree.png" alt="개인 정보 수집/이용 및 취급 위탁 동의">
 					</label>
-					<a href="#layerClause" class="clause" data-layer="#layerClause">
+					<a href="#layerClause" class="clause" data-layer="#layerClause" data-layer-options="allClose: false; append: false;">
 						<img src="images/layer-entry-clause.png" alt="약관보기">
 					</a>
 				</div>
@@ -149,27 +142,27 @@
 					<img src="images/layer-entry-submit.png" alt="확인">
 				</button>
 				<p class="notice">
-					<img src="images/layer-entry-notice.png" alt="">
+					<img src="images/layer-entry-notice.png" alt="" class="popup-image">
 					<span class="for-a11y">
 						[이벤트 유의사항]<br>
 						- 샘플은 참여 시 최초 1회만 제공되며, 참여자에 한해 주 1회 발송 됩니다<br>
 						- 해당 이벤트는 경품 소진시 조기 종료 될 수 있습니다
 					</span>
 				</p>
-			</div>
+			</section>
 		</div>
 
 		<!-- layer : 약관 -->
 		<div class="layer-wrap layer-clause" id="layerClause">
-			<div class="layer layer--medium">
+			<section class="layer layer--medium">
 				<button type="button" class="layer-close js-layer-close">
 					<span class="for-a11y">약관 레이어 팝업 닫기</span>
 				</button>
 				<h1 class="title">
-					<img src="images/layer-clause-title.png" alt="개인 정보 수집/이용 및 취급 위탁 약관">
+					<img src="images/layer-clause-title.png" alt="개인 정보 수집/이용 및 취급 위탁 약관" class="popup-image">
 				</h1>
 				<h2 class="sub-title">
-					<img src="images/layer-clause-sub-title-01.png" alt="개인 정보 수집 및 이용약관">
+					<img src="images/layer-clause-sub-title-01.png" alt="개인 정보 수집 및 이용약관" class="popup-image">
 				</h2>
 				<div class="clause">
 					멘소래담(이하 "당사"라 한다) 본 이벤트를 위하여 귀하의 개인정보를 수집 및 이용합니다.<br><br>
@@ -184,7 +177,7 @@
 					3. 귀하께서 이벤트를 통해 작성하신 내용은 해당 사이트 내에
 				</div>
 				<h2 class="sub-title">
-					<img src="images/layer-clause-sub-title-02.png" alt="개인 정보 취급 위탁 약관">
+					<img src="images/layer-clause-sub-title-02.png" alt="개인 정보 취급 위탁 약관" class="popup-image">
 				</h2>
 				<div class="clause">
 					당사는 원활한 행사 진행을 위하여 아래의 전문 업체에 업무를 위탁 처리하고 있습니다.<br><br>
@@ -199,7 +192,10 @@
 				<button type="button" class="agree">
 					<img src="images/layer-clause-agree.png" alt="동의합니다">
 				</button>
-			</div>
+			</section>
+		</div>
+		<div id="layer" style="display:none;position:fixed;overflow:hidden;z-index:9999;-webkit-overflow-scrolling:touch;">
+			<img src="//i1.daumcdn.net/localimg/localimages/07/postcode/320/close.png" id="btnCloseLayer" style="width:7%;cursor:pointer;position:absolute;right:-3px;top:-3px;z-index:1" onclick="closeDaumPostcode()" alt="닫기 버튼">
 		</div>
 		<script>
 			var search_zipcode 	= "";
@@ -286,7 +282,7 @@
 						"quatrain04"		: quatrain04,
 						"quatrain_name"		: quatrain_name
 					},
-					url: "./main_exec.php",
+					url: "../main_exec.php",
 					success: function(response){
 						if (response == "Y")
 						{
@@ -304,41 +300,78 @@
 			});
 
 			$(".search").on("click", function(){
+				// sample2_execDaumPostcode();
+				element_layer = document.getElementById('layer');
+
 				new daum.Postcode({
 					oncomplete: function(data) {
-						// 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+						// 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 			
-						// 도로명 주소의 노출 규칙에 따라 주소를 조합한다.
+						// 각 주소의 노출 규칙에 따라 주소를 조합한다.
 						// 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-						var fullRoadAddr = data.roadAddress; // 도로명 주소 변수
-						var extraRoadAddr = ''; // 도로명 조합형 주소 변수
+						var fullAddr = data.address; // 최종 주소 변수
+						var extraAddr = ''; // 조합형 주소 변수
 			
-						// 법정동명이 있을 경우 추가한다. (법정리는 제외)
-						// 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-						if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-							extraRoadAddr += data.bname;
-						}
-						// 건물명이 있고, 공동주택일 경우 추가한다.
-						if(data.buildingName !== '' && data.apartment === 'Y'){
-						extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-						}
-						// 도로명, 지번 조합형 주소가 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-						if(extraRoadAddr !== ''){
-							extraRoadAddr = ' (' + extraRoadAddr + ')';
-						}
-						// 도로명, 지번 주소의 유무에 따라 해당 조합형 주소를 추가한다.
-						if(fullRoadAddr !== ''){
-							fullRoadAddr += extraRoadAddr;
+						// 기본 주소가 도로명 타입일때 조합한다.
+						if(data.addressType === 'R'){
+							//법정동명이 있을 경우 추가한다.
+							if(data.bname !== ''){
+								extraAddr += data.bname;
+							}
+							// 건물명이 있을 경우 추가한다.
+							if(data.buildingName !== ''){
+								extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+							}
+							// 조합형주소의 유무에 따라 양쪽에 괄호를 추가하여 최종 주소를 만든다.
+							fullAddr += (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
 						}
 			
-						// 우편번호와 주소 정보를 해당 필드에 넣는다.
+						zipcode	= data.zonecode;
+						addr1		= fullAddr;
 						// document.getElementById('mb_zipcode').value = data.zonecode; //5자리 새우편번호 사용
-						document.getElementById('mb_addr1').value 	= "(" + data.zonecode + ") " + fullRoadAddr;
+						// document.getElementById('mb_addr1').value = "("+zipcode+") "+addr1;
+						// document.getElementById('mb_addr1').value = addr1;
+						document.getElementById('mb_addr1').value 	= "(" + data.zonecode + ") " + addr1;
+						// document.getElementById('mb_addr1').value 	= addr1;
 						search_zipcode 	= data.zonecode;
-						search_addr1 	= fullRoadAddr;
-					}
-				}).open();
+						search_addr1 	= addr1;
+			
+						// iframe을 넣은 element를 안보이게 한다.
+						// (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
+						element_layer.style.display = 'none';
+					},
+					width : '100%',
+					height : '100%'
+				}).embed(element_layer);
+			
+				// iframe을 넣은 element를 보이게 한다.
+				element_layer.style.display = 'block';
+			
+				// iframe을 넣은 element의 위치를 화면의 가운데로 이동시킨다.
+				initLayerPosition();
+			
 			});
+
+			function closeDaumPostcode() {
+				// iframe을 넣은 element를 안보이게 한다.
+				element_layer.style.display = 'none';
+			}
+
+			function initLayerPosition(){
+				// var width = 300; //우편번호서비스가 들어갈 element의 width
+				var width = $(window).width()*0.94; //우편번호서비스가 들어갈 element의 width
+				var height = 360; //우편번호서비스가 들어갈 element의 height
+				var borderWidth = 5; //샘플에서 사용하는 border의 두께
+
+				// 위에서 선언한 값들을 실제 element에 넣는다.
+				element_layer.style.width = width + 'px';
+				element_layer.style.height = height + 'px';
+				element_layer.style.border = borderWidth + 'px solid';
+				// 실행되는 순간의 화면 너비와 높이 값을 가져와서 중앙에 뜰 수 있도록 위치를 계산한다.
+				element_layer.style.left = (((window.innerWidth || document.documentElement.clientWidth) - width)/2) + 'px';
+				element_layer.style.top = (((window.innerHeight || document.documentElement.clientHeight) - height)/2 - borderWidth) + 60 + 'px';
+			}
+
 		</script>
 	</body>
 </html>
---
+++ m/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ko-KR">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>하다라보 고쿠쥰 4행시 이벤트</title>
+		<link type="text/css" rel="stylesheet" href="css/swiper.css">
+		<link type="text/css" rel="stylesheet" href="css/font.css">
+		<link type="text/css" rel="stylesheet" href="css/reset.css">
+		<link type="text/css" rel="stylesheet" href="css/ui.css">
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="js/swiper.min.js"></script>
+		<script type="text/javascript" src="js/ui.js"></script>
+        <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script>
+	</head>
